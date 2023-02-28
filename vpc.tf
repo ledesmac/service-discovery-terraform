a02b5a044a56aaaf6b151d8f17838dc92913c7da
Alex Ledesma <alex.ledesma7@gmail.com>
2023-02-27 17:20:42 -0700

    continuing to build on the vpc infrastructure for this demo

--- vpc.tf
+++ vpc.tf
@@ -12,3 +12,60 @@ resource "aws_subnet" "east1b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 }
+
+
+resource "aws_network_acl" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0 
+  }
+}
+
+
+resource "aws_security_group" "allow_http_tls" {
+  name        = "allow_http_tls"
+  description = "Allow TLS and HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+}
+
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  to_port           = 0
+  from_port         = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_http_tls.id
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  to_port           = 80
+  from_port         = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_http_tls.id
+}
+
+resource "aws_security_group_rule" "allow_tls" {
+  type              = "ingress"
+  to_port           = 443
+  from_port         = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_http_tls.id
+}
